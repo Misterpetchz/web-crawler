91d043f5790b351367439a68a7d5527e9a71c501
First <watcharin9581@gmail.com>
2024-09-30 02:04:26 +0700

    feat: export csv

--- app/app.py
+++ app/app.py
@@ -1,11 +1,35 @@
-from flask import Flask, render_template
+import csv
+import io
 
+from crawl import crawling  # Import your crawler function
+from flask import Flask, render_template, request, send_file
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def home():
-     return render_template('index.html')
+    return render_template("index.html")
+
+
+@app.route("/download")
+def download_csv():
+    items = crawling()
+
+    output = io.StringIO()
+    writer = csv.writer(output)
+    writer.writerow(["Item Name"])
+    for item in items:
+        writer.writerow([item["name"]])
+
+    output.seek(0)
+    return send_file(
+        io.BytesIO(output.getvalue().encode("utf-8")),
+        mimetype="text/csv",
+        download_name="items.csv",
+        as_attachment=True,
+    )
+
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
