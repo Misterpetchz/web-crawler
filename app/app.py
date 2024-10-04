999d117007ee868f97f8da10328abc004d34f496
Your Name <kinggotothemoon@gmail.com>
2024-10-05 06:41:36 +0700

    adjust styles.css and app.py by add def all_items,def get_crawled_items and html file(index_save.html,test,all_items) then add folder and file which store image and font ,the final change is adjust some part of index.html to link all_items.html

--- app/app.py
+++ app/app.py
@@ -2,9 +2,10 @@ from flask import Flask, render_template, request, send_file, jsonify
 import csv,io
 from crawl import crawling  # Import your crawler function
 
+
 app = Flask(__name__)
 
-
+session={}
 @app.route("/")
 def home():
     return render_template("index.html")
@@ -32,8 +33,27 @@ def download_csv():
 @app.route('/start-crawl', methods=['POST'])
 def start_crawl():
     items = crawling()
+    
+    session['crawled_items'] = items
+    # print(items)
+    # print(jsonify(items))
+    return jsonify(items)
+
+
+@app.route("/all-items")
+def all_items():
+    
+    return render_template("all_items.html")
+
+@app.route('/get-crawled-items', methods=['GET'])
+def get_crawled_items():
+    items = session.get('crawled_items', [])  # Retrieve the crawled items from the session
     return jsonify(items)
 
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
+
+
+# test
+
